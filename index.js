9984e4f82750a6271fe94cce2bd1a7ade497b402
pashangshangpo <pashangshangpo@gmail.com>
2020-09-05 15:40:51 +0800

    chore: 添加支持ssl

--- index.js
+++ index.js
@@ -1,11 +1,18 @@
-const http = require('http')
+const fs = require('fs')
+const https = require('https')
 const httpProxy = require('http-proxy')
+
+const options = {
+  key: fs.readFileSync('./ssl/web.xxx.ssl.key', 'utf8'),
+  cert: fs.readFileSync('./ssl/web.xxx.ssl.pem', 'utf8')
+}
 
 const proxy = httpProxy.createProxyServer({
   secure: false,
+  ssl: options
 })
 
-const server = http.createServer(function(req, res) {
+const server = https.createServer(options, function(req, res) {
   res.oldWriteHead = res.writeHead
   res.writeHead = function(statusCode, headers) {
     res.setHeader('Access-Control-Allow-Credentials', true)
@@ -17,6 +24,12 @@ const server = http.createServer(function(req, res) {
   const url = decodeURIComponent(req.url.slice(1))
 
   req.url = '/'
+
+  if (url === '' || !url.includes('http')) {
+    res.end('Hi...')
+
+    return
+  }
 
   proxy.web(req, res, { target: url, changeOrigin: true, })
 })
