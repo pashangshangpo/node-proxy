1a71af7b429ba00ae2a5f98d374d3e12ddf38547
pashangshangpo <pashangshangpo@gmail.com>
2022-04-13 16:30:57 +0800

    chore: 去除ssl证书

--- index.js
+++ index.js
@@ -1,24 +1,23 @@
-const fs = require('fs')
-const https = require('https')
+const http = require('http')
 const httpProxy = require('http-proxy')
-
-const options = {
-  key: fs.readFileSync('./ssl/web.xxx.ssl.key', 'utf8'),
-  cert: fs.readFileSync('./ssl/web.xxx.ssl.pem', 'utf8')
-}
 
 const proxy = httpProxy.createProxyServer({
   secure: false,
-  ssl: options
 })
 
-const server = https.createServer(options, function(req, res) {
+proxy.on('proxyReq', function(proxyReq) {
+  proxyReq.setHeader('user-agent', 'chrome')
+  proxyReq.setHeader('connection', 'keep-alive')
+})
+
+const server = http.createServer({}, function(req, res) {
   res.oldWriteHead = res.writeHead
+
   res.writeHead = function(statusCode, headers) {
     res.setHeader('Access-Control-Allow-Credentials', true)
     res.setHeader('Access-Control-Allow-Origin', req.headers.origin || '*')
 
-    res.oldWriteHead(statusCode, headers)
+    res.oldWriteHead(statusCode === 302 ? 200 : statusCode, headers)
   }
 
   const url = decodeURIComponent(req.url.slice(1))
@@ -31,7 +30,17 @@ const server = https.createServer(options, function(req, res) {
     return
   }
 
-  proxy.web(req, res, { target: url, changeOrigin: true, })
+  proxy.web(req, res, {
+    target: url,
+    changeOrigin: true,
+    timeout: 1000 * 10,
+    proxyTimeout: 1000 * 10,
+    followRedirects: false,
+  }, () => {
+    res.end('')
+  })
 })
 
-server.listen(9988)
+server.listen(9002, '0.0.0.0', () => {
+  console.log('http://localhost:9002')
+})
